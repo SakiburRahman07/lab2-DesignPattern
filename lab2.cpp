8995bb73e81d6d6c91e96bfef637b8d33dc546b8
asif <asifrahman00010@gmail.com>
2024-01-24 08:42:45 +0600

    shape square and rectangle class added

--- lab2.cpp
+++ lab2.cpp
@@ -1,5 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+class shape{
+public:
+ virtual void draw()=0;
+};
+
+class rectangle : public shape{
+    public:
+    void draw()
+    {
+        cout<<"Inside Rectangle"<<endl;
+    }
+};
+
+class square : public shape{
+public:
+    void draw()
+    {
+        cout<<"Inside Draw"<<endl;
+    }
+};
+
 int main()
 {
 
